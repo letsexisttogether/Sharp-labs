ee2d7eda0ca5377530fbd98bdfc8c367061e810b
Alexsander Chernenko <71719320+letsexisttogether@users.noreply.github.com>
2022-06-28 11:01:50 +0300

    Add files via upload

---
+++ MainClasses/Passport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MainClasses
+{
+    public class Passport
+    {
+        public Person PassportOwner { private get; init; } = new();
+        public string Number { get; init; } = string.Empty;
+        public string RegestrationAddress { get; init; } = string.Empty;
+        public DateOnly ActiveByDate { get; init; } = new();
+
+        public override string ToString()
+        {
+            return $"Користувач: {PassportOwner.ToString()}\nПаспортні дані: " +
+                $"Номер: {Number} Адреса реєстрації: {RegestrationAddress} " +
+                $"Дійсний до: {ActiveByDate}";
+        }
+    }
+}
